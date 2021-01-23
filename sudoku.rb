c4909f776e1e414e3710e9c28b19d525fd074123
Daedalus Phyliss <dphyliss@mi-w5.msk.21-school.ru>
2021-01-23 15:12:26 +0300

    first work version)

--- sudoku.rb
+++ sudoku.rb
@@ -1,9 +1,45 @@
+=begin
+rate moddle
+"[ [0,0,0,0,0,0,0,0,9],
+	[0,0,0,0,0,0,0,0,6],
+	[0,0,0,0,0,0,1,2,3], 
+	[0,0,0,0,0,5,9,6,7], 
+	[0,0,7,3,1,2,8,4,5], 
+	[0,0,5,0,0,0,0,0,2], 
+	[0,0,1,5,7,4,6,9,8], 
+	[0,0,8,2,3,0,5,0,4], 
+	[0,0,0,9,0,8,0,3,0] ]"
+
+rate expert
+"[ [0,7,2,0,0,5,0,0,0],
+	[9,0,0,0,0,0,0,8,0],
+	[0,0,0,0,7,0,0,0,0], 
+	[0,0,0,2,0,0,3,0,5], 
+	[3,0,0,0,1,0,7,0,0], 
+	[0,0,0,0,0,4,8,0,0], 
+	[1,0,0,0,0,6,0,0,9], 
+	[0,0,6,1,0,0,0,0,0], 
+	[0,0,0,3,4,0,0,0,2] ]"
+
+rate hard
+"[ [2,0,0,4,0,0,8,0,0],
+	[0,8,0,3,0,0,6,0,0],
+	[0,0,3,0,0,0,0,0,0], 
+	[0,0,0,0,2,9,0,0,1], 
+	[0,0,9,0,5,1,0,0,6], 
+	[0,6,0,0,0,3,9,0,2], 
+	[0,0,0,0,0,0,0,7,0], 
+	[0,4,0,0,8,0,0,0,0], 
+	[1,9,0,0,0,0,0,4,0] ]"
+=end
+
 require 'json'
 
-
-# valid "[[1,2,3,4,5,6,7,8,9], [7,8,9,1,2,3,4,5,6], [4,5,6,7,8,9,1,2,3], [3,1,2,8,4,5,9,6,7], [6,9,7,3,1,2,8,4,5], [8,4,5,6,9,7,3,1,2], [2,3,1,5,7,4,6,9,8], [9,6,8,2,3,1,5,7,4], [5,7,4,9,6,8,2,3,1]]"
-
-# invalid "[[1,2,3,4,5,6,7,8,9], [7,8,9,1,2,3,4,5,6], [4,5,6,7,8,9,1,2,3], [3,1,2,8,4,5,9,6,7], [6,9,7,3,1,2,8,4,5], [8,4,5,6,9,7,3,1,2], [2,3,1,5,7,4,6,9,8], [9,6,8,2,3,1,5,7,4], [5,7,4,9,6,8,2,3,100]"
+MIN = 0..2
+MID = 3..5
+MAX = 6..8
+UNDEFINE = 0
+$solution = 0
 
 def exit_sudoku(err_str)
 	puts err_str
@@ -11,130 +47,121 @@ def exit_sudoku(err_str)
 end
 
 def gorizont_check(mas, line)
-	i = 0;
-	while (i < 9) do
-		j = i + 1
-		while (j < 9) do
-			if ((mas[line][i] == mas[line][j]) && (mas[line][i] != 0))
-				exit_sudoku("invalid line")
+	0.upto 8 do |i|
+		(i + 1).upto 8 do |j|
+			if ((mas[line][i] == mas[line][j]) && (mas[line][i] != UNDEFINE))
+				return false
 			end
-			j += 1
 		end
-		i += 1
 	end
-	# mas[line].each do |elem| 
-
-	# 		if mas[line][i] == mas[line][j]
-	# 			exit_sudoku("invalid line")
-	# 		end
-	# 	end
-	# end
-
-	# для нулей не подходит 
-	# if  mas[line].uniq.length != mas[line].length
-	# 	exit_sudoku("invalid line")
-	# end
+	return true
 end
 
 def vert_check(mas, сcolumn)
-	i = 0;
-	while (i < 9) do
-		j = i + 1
-		while (j < 9) do
-			if ((mas[i][сcolumn] == mas[j][сcolumn]) && (mas[i][сcolumn] != 0))
-				exit_sudoku("invalid column")
+	0.upto 8 do |i|
+		(i + 1).upto 8 do |j|
+			if ((mas[i][сcolumn] == mas[j][сcolumn]) && (mas[i][сcolumn] != UNDEFINE))
+				return false
 			end
-			j += 1
 		end
-		i += 1
 	end
-	
+	return true
 end
 
-def digital_check(mas)
-	mas.each do |column|  
-		column.each do |elem|  
-			if ((elem < 0) || (9 < elem))
-				exit_sudoku("invalid digital")
-			end
-		end
+def square_limits(index)
+	case index
+		when MIN
+			return MIN.min, MIN.max
+		when MID
+			return MID.min, MID.max
+		when MAX
+			return MAX.min, MAX.max
 	end
 end
 
 def square_check(mas, i, j)
-	# i += 1;
-	# j += 1;
-	# from_i =  i % 3 == 0 ?  i - 3 : i - (i % 3)
-	# to_i =  i % 3 == 0 ?  i - 1 : i + (i % 3)
-	case i
-		when 0..2
-			from_i = 0
-			to_i = 2
-		when 3..5
-			from_i = 3
-			to_i = 5
-		when 6..8
-			from_i = 6
-			to_i = 8
-	end
-	case j
-		when 0..2
-			from_j = 0
-			to_j = 2
-		when 3..5
-			from_j = 3
-			to_j = 5
-		when 6..8
-			from_j = 6
-			to_j = 8
-	end
-	buff = []
-	while (from_i <= to_i) do
-		while (from_j <= to_j) do
-			buff.push(mas[from_i][from_j])
-			from_j += j
+	from_i, to_i = square_limits(i)
+	from_j, to_j = square_limits(j)
+	square_values = []
+	from_i.upto to_i do |ii|
+		from_j.upto to_j do |jj|
+			square_values.push(mas[ii][jj])
 		end
-		from_i += 1
 	end
+	square_values.delete UNDEFINE
+	if (square_values.size != square_values.uniq.size)
+		return false
+	end
+	return true
+end
 
-	buff.each_with_index do |val, index|
-		buff.drop(index + 1).each do |value| 
-			if (val == value)
-				exit_sudoku("invalid square")
-			end
-		end
-	end
+def sudoku_print(mas)
+	puts 
+	mas.each {|column| puts column.inspect}
 end
 
 def sudoku_check(mas)
-	
+	rezult = true
+	9.times do |x| 
+		rezult = (gorizont_check(mas, x) && vert_check(mas, x) && square_check(mas, x, x)) ? rezult : false;
+	end
+	return rezult
+end
+
+def search_zero(mas)
+	0.upto 8 do |i|
+		0.upto 8 do |j|
+			if (0 == mas[i][j])
+				return i, j
+			end
+		end
+	end
+	return nil
+end
+
+def possible_values(mas, i, j)
+	values = []
+	1.upto 9 do |val|
+		mas[i][j] = val
+		if (square_check(mas, i, j) && gorizont_check(mas, i) && vert_check(mas, j))
+			values.push(val)
+		end
+		mas[i][j] = UNDEFINE
+	end
+	if (0 == values.size)
+		return nil
+	end
+	return values
+end
+
+def recursive_search(mas)
+	i, j = search_zero(mas)
+	if (nil == i)
+		sudoku_print(mas)
+		$solution += 1
+		return nil
+	end
+	values = possible_values(mas, i, j)
+	if (nil == values)
+		return nil
+	end
+	values.each do |val|
+		duplicate = Marshal.load(Marshal.dump(mas))
+		duplicate[i][j] = val
+		recursive_search(duplicate)
+	end
 end
 
 mas = JSON.parse ARGV[0]
 
 if (mas.class != Array)
 	exit_sudoku("Couldn't read the array")
-end 
-
-9.times do |x| 
-	gorizont_check(mas, x)
-	vert_check(mas, x)
+elsif (false == sudoku_check(mas))
+	exit_sudoku("Invalid input params")
 end
 
+recursive_search(mas)
 
-digital_check(mas)
-square_check(mas, 0, 0)
-square_check(mas, 3, 0)
-square_check(mas, 6, 0)
-square_check(mas, 0, 3)
-square_check(mas, 0, 6)
-square_check(mas, 3, 3)
-square_check(mas, 3, 6)
-square_check(mas, 6, 3)
-square_check(mas, 6, 6)
-
-# print mas.inspect
-
-
-
-
+if (0 == $solution)
+	exit_sudoku("Invalid input params or sudoku don't solution")
+end
